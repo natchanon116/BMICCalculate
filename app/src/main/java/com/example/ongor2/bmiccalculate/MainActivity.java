8cc2a33a5a48ea11b7c596a9b47a708178ab5af4
natchanon116 <pangzaasf@gmail.com>
2019-01-07 11:26:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ongor2/bmiccalculate/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ongor2.bmiccalculate;
+
+import android.icu.text.DecimalFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button_cal = (Button)findViewById(R.id.button_cal);
+        button_cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calBMI();
+
+            }
+        });
+    }
+    private void calBMI(){
+        EditText textw = (EditText)findViewById(R.id.editText_weigth);
+        EditText texth = (EditText)findViewById(R.id.editText_hrigth);
+
+        double w = Double.parseDouble(textw.getText().toString());
+        double h = Double.parseDouble(texth.getText().toString());
+
+        h = h/100; // cm--->m
+        double bmi = w/ (h*h);
+        DecimalFormat decFormat = new DecimalFormat( "##.##");
+        String msg = "ค่า BMI คือ:" + decFormat.format(bmi) + "\n";
+        msg += "ลักษณะรูปร่างคือ:";
+
+        if(bmi >= 40) msg += "อ้วนขั้นสูงสุด";
+        else if(bmi >= 35) msg += "อ้วนขั้นที่ 2";
+        else if(bmi >= 28.5) msg += "อ้วนขั้นที่ 1";
+        else if(bmi >= 23.5) msg += "น้ำหนักเกิน";
+        else msg += "อยุ่ในเกณฑ์ปกติ";
+
+        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_LONG).show();
+    }
+}
